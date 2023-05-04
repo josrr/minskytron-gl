3cc58eec4db0f10829daeb9e4d5d657d8da65c0f
José M. Á. Ronquillo Rivera <josrr@ymail.com>
2023-05-04 17:06:02 -0600

    + the package SHADERS no longer use OPENGL

--- package.lisp
+++ package.lisp
@@ -1,10 +1,10 @@
 ;;;; package.lisp
 
 (defpackage #:shaders
-  (:use #:cl #:cl-opengl)
-  (:export #:con-shader
-	   #:compila-shader
-           #:lee-shader-de-archivo))
+  (:use #:cl)
+  (:export #:with-shaders
+	   #:compile-shaders
+           #:read-shader-from-file))
 
 (defpackage #:minskytron-gl
   (:use #:cl #:cl-opengl))
